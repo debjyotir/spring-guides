1e900d5a80da8c3b870888da054fe232cade94e1
debjyotir <droy@chitika.com>
2016-08-18 13:07:58 +0530

    Complete code

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,4 +4,17 @@ package hello;
  * Created by deb on 18/8/16.
  */
 public class Greeting {
+    private final long id;
+    private final String content;
+    public Greeting(long id, String content){
+        this.id=id;
+        this.content=content;
+    }
+    public long getId(){
+        return id;
+    }
+    public String getContent(){
+        return content;
+    }
+
 }
