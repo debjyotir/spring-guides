53499187df6f81e7c8cdc1f74d4bba2f59120c67
Debjyoti Roy <debjyotiroy2@gmail.com>
2016-08-21 10:35:26 +0530

    validating form input

---
+++ src/main/java/validating_form_input/Application.java
@@ -0,0 +1,14 @@
+package validating_form_input;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by debjyoti roy on 21-08-2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ src/main/java/validating_form_input/PersonForm.java
@@ -0,0 +1,38 @@
+package validating_form_input;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by debjyoti roy on 20-08-2016.
+ */
+public class PersonForm {
+    @Size(min=2,max=30)
+    private String name;
+
+    @NotNull
+    @Min(18)
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "Person(Name: " + this.name + ", Age: " + this.age + ")";
+    }
+}
